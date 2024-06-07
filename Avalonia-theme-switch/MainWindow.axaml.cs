6ec925dc5ffddc1b0f478f6447d59037007b8f6f
hallyoung <ohjunk2001@outlook.com>
2024-06-07 12:41:53 +0800

    1. add cloud and sun to ToggleButton 2. set basic style

---
+++ Avalonia-theme-switch/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia_theme_switch;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
